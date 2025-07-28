e6aee4e79b443a09045f08c0eb4619877582c6d4
ytech911 <vavapoirier2002@gmail.com>
2025-07-28 21:36:40 +0200

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,5 @@ def serve_index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    # Render attribue automatiquement le port via la variable d'environnement PORT
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 10000))  # Render fournit ce port
     app.run(host='0.0.0.0', port=port)
