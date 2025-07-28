bf717ec85433fcb37e638a3a7c5ed1e789a8be12
ytech911 <vavapoirier2002@gmail.com>
2025-07-28 21:30:13 +0200

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, send_from_directory
+
 app = Flask(__name__, static_folder='.')
 
 @app.route('/')
-def serve_html():
+def serve_index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
